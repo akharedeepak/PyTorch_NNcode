d2fa8211479704506b837694c3ac4080aed00660
akharedeepak <42086011+akharedeepak@users.noreply.github.com>
2021-08-18 22:25:06 -0400

    Add files via upload

---
+++ RNN_CharacterLevel/CharacterLevel_RNN.py
@@ -0,0 +1,191 @@
+from __future__ import unicode_literals, print_function, division
+from io import open
+import glob
+import os
+from numpy.core.numeric import outer
+import matplotlib.pyplot as plt
+
+from torch._C import set_flush_denormal
+from torch.cuda import random
+from torch.nn.functional import softmax
+from torch.optim import optimizer
+
+def findFiles(path): return glob.glob(path)
+
+import unicodedata
+import string
+
+all_letters = string.ascii_letters + " .,;'"
+n_letters = len(all_letters)
+
+# Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+#print(unicodeToAscii('Ślusàrski'))
+
+# Build the category_lines dictionary, a list of names per language
+category_lines = {}
+all_categories = []
+
+# Read a file and split into lines
+def readLines(filename):
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+    return [unicodeToAscii(line) for line in lines]
+
+for filename in findFiles('data/names/*.txt'):
+    category = os.path.splitext(os.path.basename(filename))[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+n_categories = len(all_categories)
+
+#print(category_lines['Italian'][:5])
+
+import torch
+
+# Find letter index from all_letters, e.g. "a" = 0
+def letterToIndex(letter):
+    return all_letters.find(letter)
+
+# Just for demonstration, turn a letter into a <1 x n_letters> Tensor
+def letterToTensor(letter):
+    tensor = torch.zeros(1, n_letters)
+    tensor[0][letterToIndex(letter)] = 1
+    return tensor
+
+# Turn a line into a <line_length x 1 x n_letters>,
+# or an array of one-hot letter vectors
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
+
+#print(letterToTensor('J'))
+
+#print(lineToTensor('Jones').size())
+
+import random
+
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+def randomTrainingExample():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long)
+    line_tensor = lineToTensor(line)
+    return category, line, category_tensor, line_tensor
+
+for i in range(10):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    #print('category =', category, '/ line =', line)
+
+########################################################################################3
+
+import torch.nn as nn
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size+hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size+hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input_tensor, hidden_tensor):
+        combined = torch.cat((input_tensor, hidden_tensor), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def init_hidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+
+def category_from_output(output):
+    category_idx = torch.argmax(output).item()
+    return all_categories[category_idx]
+
+
+n_hidden = 128
+rnn = RNN(n_letters, n_hidden, n_categories)
+
+criterion = nn.NLLLoss()
+learning_rate = 0.005
+optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
+
+def train(line_tensor, category_tensor):
+    hidden = rnn.init_hidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    loss = criterion(output, category_tensor)
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return output, loss.item()
+
+current_loss = 0
+all_losses = []
+plot_step, print_step = 1000, 5000
+n_iter = 100000
+
+
+for i in range(n_iter):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+
+    output, loss = train(line_tensor, category_tensor)
+    current_loss += loss
+
+    if (i+1) % plot_step == 0:
+        all_losses.append(current_loss / plot_step)
+        current_loss = 0
+
+    if (i+1) % print_step == 0:
+        guess = category_from_output(output)
+        correct = "CORRECT" if guess == category else f"WRONG ({category})"
+        print(f'{i} {i/n_iter*100} {loss:.4f} {line} / {guess} {correct}')
+
+plt.figure()
+plt.plot(all_losses)
+plt.show()
+
+def evaluate(line_tensor):
+    hidden = rnn.init_hidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    return output
+
+
+def predict(input_line, n_predictions=3):
+    print('\n> %s' % input_line)
+    with torch.no_grad():
+        output = evaluate(lineToTensor(input_line))
+
+        # Get top N categories
+        topv, topi = output.topk(n_predictions, 1, True)
+        predictions = []
+
+        for i in range(n_predictions):
+            value = topv[0][i].item()
+            category_index = topi[0][i].item()
+            print('(%.2f) %s' % (value, all_categories[category_index]))
+            predictions.append([value, all_categories[category_index]])
+
+predict('Dovesky')
+predict('Jackson')
+predict('Satoshi')
